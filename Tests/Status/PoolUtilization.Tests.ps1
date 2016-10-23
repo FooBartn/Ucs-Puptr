ebe954350753eefcf3288e792ba19bf36ae921e8
FooBartn <barton.joshua@gmail.com>
2016-10-23 18:21:18 -0500

    Created a loop for the It statement in this test

--- Tests/Status/PoolUtilization.Tests.ps1
+++ Tests/Status/PoolUtilization.Tests.ps1
@@ -31,63 +31,32 @@ Process {
 
         # Run test case
         foreach ($UcsDomain in (Get-UcsStatus)) {
-            It -Name "$($UcsDomain.Name) is using less than $PoolThreshold percent of the MAC Pool" -Test {
-                #
-                # Run commands to gather data
-                $MacPools = @(Get-UcsMacPool | Where {$_.Size -ne 0})
 
-                # Assert
-                try {
-                    foreach ($MacPool in $MacPools) {
-                        ($MacPool.Assigned * 100 / $MacPool.Size) | Should Not BeGreaterThan $PoolThreshold
-                    }
-                } catch {
-                    throw $_
-                }
+            $UIDPools = @{
+                MacPool = Get-UcsMacPool -Ucs $UcsDomain.Name |
+                    Where {$_.Size -ne 0}
+                UuidPool = Get-UcsUuidSuffixPool -Ucs $UcsDomain.Name |
+                    Where {$_.Size -ne 0}
+                WwnnPool = Get-UcsWwnPool -Ucs $UcsDomain.Name |
+                    Where {$_.Purpose -eq 'node-wwn-assignment' -AND $_.Size -ne 0}
+                WwpnPool = Get-UcsWwnPool -Ucs $UcsDomain.Name | 
+                    Where {$_.Purpose -eq 'port-wwn-assignment'-AND $_.Size -ne 0}
             }
 
-            It -Name "$($UcsDomain.Name) is using less than $PoolThreshold percent of the UUID Pool" -Test {
-                #
-                # Run commands to gather data
-                $UuidPools = @(Get-UcsUuidSuffixPool | Where {$_.Size -ne 0})
+            foreach ($PoolType in $UIDPools) {
+                It -Name "$($UcsDomain.Name) is using less than $PoolThreshold percent of the MAC Pool" -Test {
+                    #
+                    # Run commands to gather data
+                    $MacPools = @(Get-UcsMacPool | Where {$_.Size -ne 0})
 
-                # Assert
-                try {
-                    foreach ($UuidPool in $UuidPools) {
-                        ($UuidPool.Assigned * 100 / $UuidPool.Size) | Should Not BeGreaterThan $PoolThreshold
+                    # Assert
+                    try {
+                        foreach ($UIDPool in $UIDPools.$PoolType) {
+                            ($UIDPool.Assigned * 100 / $UIDPool.Size) | Should Not BeGreaterThan $PoolThreshold
+                        }
+                    } catch {
+                        throw $_
                     }
-                } catch {
-                    throw $_
-                }
-            }
-
-            It -Name "$($UcsDomain.Name) is using less than $PoolThreshold percent of the WWNN Pool" -Test {
-                #
-                # Run commands to gather data
-                $WwnnPools = @(Get-UcsWwnPool |  Where {$_.Purpose -eq 'node-wwn-assignment' -AND $_.Size -ne 0})
-
-                # Assert
-                try {
-                    foreach ($WwnnPool in $WwnnPools) {
-                        ($WwnnPool.Assigned * 100 / $WwnnPool.Size) | Should Not BeGreaterThan $PoolThreshold
-                    }
-                } catch {
-                    throw $_
-                }
-            }
-
-            It -Name "$($UcsDomain.Name) is using less than $PoolThreshold percent of the WWPN Pool" -Test {
-                #
-                # Run commands to gather data
-                $WwpnPools = @(Get-UcsWwnPool | Where {$_.Purpose -eq 'port-wwn-assignment'-AND $_.Size -ne 0})
-
-                # Assert
-                try {
-                    foreach ($WwpnPool in $WwpnPools) {
-                        ($WwpnPool.Assigned * 100 / $WwpnPool.Size) | Should Not BeGreaterThan $PoolThreshold
-                    }
-                } catch {
-                    throw $_
                 }
             }
         }
