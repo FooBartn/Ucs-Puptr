fd3251c33350f97d42d6b3c30e35ad39105d079f
FooBartn <barton.joshua@gmail.com>
2016-10-26 17:33:29 -0500

    Added EnabledDownPorts Test
    Merged EnabledLan and EnabledSan tests into EnabledDownPorts

---
+++ Tests/Network/EnabledDownPorts.Tests.ps1
@@ -0,0 +1,75 @@
+#requires -Modules Pester, Cisco.UcsManager
+
+[CmdletBinding()]
+Param(
+    # Optionally fix all config drift that is discovered. Defaults to false (off)
+    [switch]$Remediate = $false,
+
+    # Optionally define a different config file to use.
+    [string]$Config
+)
+
+Process {
+    # Tests
+    Describe -Name 'Network Configuration: Ports Both Enabled and Down ' -Tag @('network','no-impact') -Fixture {
+        # Project Environment Variables      
+        $ProjectDir = (Get-Item $PSScriptRoot).parent.parent.FullName
+        $CredentialDir = "$ProjectDir\Credentials"
+        
+        # Config Variables
+        . $Config
+        [string]$PuptrUser = $config.connection.Username
+        [string[]]$UcsDomains = $config.connection.Domain
+        #[vartype]$var = 
+
+        # Importing credentials
+        $SecurePassword = Get-Content -Path "$CredentialDir\$PuptrUser.txt" | ConvertTo-SecureString
+        $Credential = [pscredential]::new($PuptrUser,$SecurePassword)
+
+        # Connect to UCS 
+        Connect-Ucs -Name $UcsDomains -Credential $Credential
+
+        # Run test case
+        foreach ($UcsDomain in (Get-UcsStatus)) {
+            $EthPorts = Get-UcsUplinkPort -Ucs $UcsDomain.Name -AdminState 'enabled' -OperState 'down'
+            $FcPorts = Get-UcsFcUplinkPort -Ucs $UcsDomain.Name -AdminState 'enabled' -OperState 'down'
+
+            It -Name "$($UcsDomain.Name) has no uplink ports both enabled and down" -Test {
+                # Assert
+                try {
+                    $EthPorts.count | Should Be 0
+                } catch {
+                    if ($Remediate) {
+                        Write-Warning -Message $_
+                        foreach ($EthPort in $EthPorts) {
+                            Write-Warning -Message "Disabling port $($EthPort.SlotId)/$($EthPort.PortId) on FI $($EthPort.SwitchId) "
+                            $EthPort | Set-UcsUplinkPort -AdminState 'disabled' -Force
+                        }
+                    } else {
+                        throw $_
+                    }
+                }
+            }
+
+            It -Name "$($UcsDomain.Name) has no fc ports both enabled and down" -Test {
+                # Assert
+                try {
+                    $FcPorts.count | Should Be 0
+                } catch {
+                    if ($Remediate) {
+                        Write-Warning -Message $_
+                        foreach ($FcPort in $FcPorts) {
+                            Write-Warning -Message "Disabling port $($FcPort.SlotId)/$($FcPort.PortId) on FI $($FcPort.SwitchId) "
+                            $FcPort | Set-UcsUplinkPort -AdminState 'disabled' -Force
+                        }
+                    } else {
+                        throw $_
+                    }
+                }
+            }
+        }
+
+        # Disconnect from UCS
+        Disconnect-Ucs
+    }
+}
